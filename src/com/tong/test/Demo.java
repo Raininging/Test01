fdbcb2eb421963c0e88244630c36d1f62748e653
Snow <zxtlut@163.com>
2017-11-30 15:07:03 +0800

    Initial commit

---
+++ src/com/tong/test/Demo.java
@@ -0,0 +1,10 @@
+package com.tong.test;
+
+/**
+ * Created by Administrator on 2017/11/30 0030.
+ */
+public class Demo {
+    public static void main(String[] args){
+      System.out.println("Hey kitty");
+    }
+}
